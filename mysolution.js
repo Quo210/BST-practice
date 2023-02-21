f7a9eaf0ab4b1be60d0aae2ffe14379059cbfda6
Gabriel <gabrielrengifo211@gmail.com>
2023-02-20 22:14:10 -0400

    finished scripts

--- mysolution.js
+++ mysolution.js
@@ -9,6 +9,7 @@ class Tree {
     constructor(arr){
         var cleansed = this.#cleanseInput(arr);
         this.root = this.buildTree(cleansed, 0, cleansed.length - 1);
+        console.log('A new tree has been created.')
     };
     #cleanseInput(arr){
         return Array.from (new Set(arr.sort((a,b)=>a-b)))
@@ -27,7 +28,7 @@ class Tree {
     };
     insert(root,key){
         if (root == null){
-            //console.log(key,'a new node was created due to ', root)
+            console.log('a new node was created using ', key)
             root = new Node(key);
             return root
         };
@@ -38,7 +39,7 @@ class Tree {
             //console.log(key,'key is bigger ',root.data)
             root.right = this.insert(root.right,key);
         } else {//If key == root.data
-            console.error('Can not have duplicate values.')
+            console.error(`Can not have duplicate values. ${key} was not added to the tree.}`)
         }
         return root
     };
@@ -50,35 +51,35 @@ class Tree {
         if (root == null){
             return null
         };
-        console.log('Case: Executing search of matching node for: ', key);
+        //console.log('Case: Executing search of matching node for: ', key);
         //find the node with the matching value
         if(key < root.data){
-            console.log('Case: Redirecting to left tree')
+            //console.log('Case: Redirecting to left tree')
             root.left = this.deleteNum(root.left,key)
         } else if (key > root.data){
-            console.log('Case: Redirecting to right tree')
-            console.log(this.deleteNum(root.right,key))
+            //console.log('Case: Redirecting to right tree')
+            //console.log(this.deleteNum(root.right,key))
             root.right = this.deleteNum(root.right,key)
-            console.log('Status of right root: ', root.right)
+            //console.log('Status of right root: ', root.right)
         } else {//Matching find. Elaborate the 3 cases
             switch(true){
                 case (root.left == null && root.right == null):
-                    console.log('Case: Node has no children')
+                    //console.log('Case: Node has no children')
                     root = null;
                     return root;
                     break;
                 case (root.left == null):
-                    console.log('Case: Node has no left children')
+                    //console.log('Case: Node has no left children')
                     root = root.right;
                     return root;
                     break;
                 case (root.right == null):
-                    console.log('Case: Node has no right children')
+                    //console.log('Case: Node has no right children')
                     root = root.left;
                     return root;
                     break;
                 default: // Node to deleteNum has at least 1 child on each side
-                console.log('Case: Node has left and right children')
+                //console.log('Case: Node has left and right children')
                 let succesor = this.findSuccesor(root.right);
                 root.data = succesor;
                 root.right = this.deleteNum(root.right,succesor);
@@ -99,7 +100,7 @@ class Tree {
         return succesor
     };
     findNum(num,root = this.root){
-        console.log('Searching',num,'on',root)
+        //console.log('Searching',num,'on',root)
         if(root == null){
             console.log('NOT FOUND')
             return root
@@ -114,7 +115,7 @@ class Tree {
             return false
         }
     };
-    levelOrder(func){
+    levelOrder(func,obj = {}){
         const traversal = [];
         const queue = [this.root];
         while (queue.length != 0){
@@ -138,7 +139,7 @@ class Tree {
             case (ref.right != null):
                 storage.push(ref.right);
             default:
-                'Wut'
+                'Should never reach here'
         };
         print += ' ' + ref.data;
         storage.shift()
@@ -197,25 +198,38 @@ class Tree {
         return `Depth for ${nodeNum} is: ` + depth
     }
     isBalanced(){
-        let status = true;
-        this.levelOrder((node,status)=>{
+        let status = { 'isBalanced' : true };
+        this.levelOrder((node)=>{
+            if (status.isBalanced == false){
+                return status
+            }
             if(node == null){
                 //do nothing
             } else {
+                //console.log(node,`node was not null`)
                 let left = this.height(node.left);
-                let right = this.height(node.left);
+                let right = this.height(node.right);
                 if(left > right){
-                    if( (left - right) > 1) status = false;
+                    let calc = left - right;
+                    //console.log(`${left} larger than ${right}`, `difference is ${calc} levels of depth`)
+                    if( calc > 1) status.isBalanced = false;
                 } else if (left < right){
-                    if( (right - left) > 1) status = false;
-                } else {//They equal
-                    true
+                    let calc = right - left;
+                    //console.log(`${right} larger than ${left}`, `difference is ${calc}}`)
+                    if( calc > 1) status.isBalanced = false;
+                } else {
+                    //console.log(`${left} is as balanced ${right}`)
                 }
             }
-        });
-        return 'is balanced?: ' + status 
+        }, status);
+        return `The current Binary Tree can be considered ${(status.isBalanced == false)? 'not balanced' : 'balanced.'}`
     };
-
+    rebalance(){
+        const data = this.inOrder();
+        this.root = this.buildTree(data, 0, data.length -1)
+        console.log('The tree has been rebalanced.')
+        return this.root
+    }
 
 };
 
@@ -241,27 +255,37 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
       prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
     }
 };
-//const ara = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324,7000,8000,9000,14,99,33,55,20,13,14,19];
-var ara = [1, 2, 3, 4, 5, 6, 7,123,11,15,23,123,123,121,1,2,5,9,10,22,89,74,21,1040,1041,2000];
-//const ara = makeRandoNumArr(200);
-const myTree = new Tree(ara);
-myTree.insert(myTree.root,67)
-myTree.insert(myTree.root,68)
-myTree.insert(myTree.root,71)
-myTree.insert(myTree.root,72)
-myTree.insert(myTree.root,73)
-myTree.insert(myTree.root,75)
-//myTree.deleteNum(myTree.root,4)
-//myTree.deleteNum(myTree.root,89)
-//myTree.deleteNum(myTree.root,123)
-//myTree.deleteNum(myTree.root,7)
-//console.log(myTree.findNum(26))
-//console.log(myTree.levelOrder(console.log))
-//console.log(myTree.recurseLvlOr())
-// console.log('PreOrder',myTree.preOrder())
-// console.log('inOrder',myTree.inOrder())
-// console.log('postOrder',myTree.postOrder())
-//console.log(myTree.findNum(7))
-//console.log(myTree.findDepth(15))
-console.log( myTree.isBalanced() )
+// 0. Create Array of random numbers
+const ara = makeRandoNumArr(30);
+// 1. Create BST
+const myTree = new Tree(ara); 
+// 2. Check and confirm the tree being balanced;
+console.log( myTree.isBalanced() ) 
+// Show tree on console
 prettyPrint(myTree.root);
+// 3. Print elements in pre, post and in order 
+console.log('Tree data read preOrder: ', myTree.preOrder())
+console.log('Tree data read postOrder: ', myTree.postOrder())
+console.log('Tree data read inOrder: ', myTree.inOrder())
+// 4. Unbalance the tree
+myTree.insert(myTree.root,167)
+myTree.insert(myTree.root,268)
+myTree.insert(myTree.root,371)
+myTree.insert(myTree.root,72)
+myTree.insert(myTree.root,173)
+myTree.insert(myTree.root,275)
+// 5. Confirm that tree is unbalanced
+console.log( myTree.isBalanced() );
+// show unbalanced tree
+prettyPrint(myTree.root);
+// 6. Balance the tree
+myTree.rebalance();
+// 7. Confirm that tree has been rebalanced
+console.log( myTree.isBalanced() );
+// show rebalanced tree
+prettyPrint(myTree.root);
+// 8. Print all elements in pre, post and in order 
+console.log('Tree data read preOrder: ', myTree.preOrder())
+console.log('Tree data read postOrder: ', myTree.postOrder())
+console.log('Tree data read inOrder: ', myTree.inOrder())
+
